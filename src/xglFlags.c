234131cd1c241d69a4604f9430b6e0023b9b47dc
osab <zrker@icloud.com>
2026-08-11 05:53:15 -0400

    Match xglFlags 64-bit wrappers

--- src/xglFlags.c
+++ src/xglFlags.c
@@ -10,9 +10,29 @@ int xglFlagsSet8(int nFlag, int nValue) { return xglFlagsSet(nFlag, 8, nValue); 
 int xglFlagsSet16(int nFlag, int nValue) { return xglFlagsSet(nFlag, 0x10, nValue); }
 int xglFlagsSet32(int nFlag, int nValue) { return xglFlagsSet(nFlag, 0x20, nValue); }
 
+long long xglFlagsSet64(int nFlag, long long nValue)
+{
+    long long nLow;
+    long long nHigh;
+
+    nLow = xglFlagsSet(nFlag, 0x20, (int)nValue);
+    nHigh = xglFlagsSet(nFlag + 0x20, 0x20, nValue >> 32);
+    return (nHigh << 32) + nLow;
+}
+
 int xglFlagsGet1(int nFlag) { return xglFlagsGet(nFlag, 1); }
 int xglFlagsGet2(int nFlag) { return xglFlagsGet(nFlag, 2); }
 int xglFlagsGet4(int nFlag) { return xglFlagsGet(nFlag, 4); }
 int xglFlagsGet8(int nFlag) { return xglFlagsGet(nFlag, 8); }
 int xglFlagsGet16(int nFlag) { return xglFlagsGet(nFlag, 0x10); }
 int xglFlagsGet32(int nFlag) { return xglFlagsGet(nFlag, 0x20); }
+
+long long xglFlagsGet64(int nFlag)
+{
+    long long nLow;
+    long long nHigh;
+
+    nLow = xglFlagsGet(nFlag, 0x20);
+    nHigh = xglFlagsGet(nFlag + 0x20, 0x20);
+    return (nHigh << 32) + nLow;
+}
