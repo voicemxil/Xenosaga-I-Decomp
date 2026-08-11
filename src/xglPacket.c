6fdd4b71b2838247f46321e9353988c64ae13642
osab <zrker@icloud.com>
2026-08-11 03:31:33 -0400

    Match xglPacketMove

--- src/xglPacket.c
+++ src/xglPacket.c
@@ -16,6 +16,10 @@ typedef struct XGLPACKET {
 } XGLPACKET;
 
 void sceVif1PkInit(XGLPACKET *pPk, u_int *pBuff);
+void sceVif1PkEnd(XGLPACKET *pPk, u_int nFlags);
+u_int sceVif1PkTerminate(XGLPACKET *pPk);
+void sceVif1PkReset(XGLPACKET *pPk);
+void FlushCache(int nMode);
 void xglDmaDirectSrcChain(u_int nCh, u_int nAddr);
 
 XGLPACKET asPacketSource[2];
@@ -39,6 +43,22 @@ void xglPacketInit(void)
     asPacketSource[1].pLimit = (u_int *)0x1000000;
     pCurrentPacket = &asPacketSource[0];
     pSendPacket = 0;
+}
+
+/* Finish the current packet and switch to the other build buffer */
+void xglPacketMove(void)
+{
+    sceVif1PkEnd(pCurrentPacket, 0);
+    sceVif1PkTerminate(pCurrentPacket);
+    FlushCache(0);
+    ((u_int *)pCurrentPacket)[9] = ((u_int *)pCurrentPacket)[8];
+    pSendPacket = pCurrentPacket;
+    if (pCurrentPacket == &asPacketSource[1]) {
+        pCurrentPacket = &asPacketSource[0];
+    } else {
+        pCurrentPacket = &asPacketSource[1];
+    }
+    sceVif1PkReset(pCurrentPacket);
 }
 
 /* Kick the finished packet to VIF1 via source-chain DMA */
